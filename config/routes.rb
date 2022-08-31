50ef066ca62e89b50a958704ff8040c718af29b2
EC2 Default User <ec2-user@ip-172-31-19-77.ec2.internal>
2022-08-31 08:45:19 +0000

    [Add] top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  # root :to => ''
+  # ルーティングの設定
+  root :to => 'homes#top'
   
   resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
